43774cc73f74a3124cc58ffb2655e37ee72c21f6
Wombat <destination@bigmir.net>
2020-04-11 20:19:16 +0300

    changed app text 4

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Xexe there feature 3
+          Xexe there feature 4
         </p>
         <a
           className="App-link"
